515aa2024be2e9b6daaf738d066e313d76e01dcd
Gihyeon Sung <gi@hyeon.io>
2020-02-19 00:43:26 +0900

    Setup tokyo-metro color scheme

--- config.def.h
+++ config.def.h
@@ -10,15 +10,15 @@ static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Iosevka:size=12", "monospace:size=10" };
 static const char dmenufont[]       = "Iosevka:size=12";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#1c1c1c";
+static const char col_gray2[]       = "#1c1c1c";
+static const char col_gray3[]       = "#d0d0d0";
+static const char col_gray4[]       = "#d0d0d0";
+static const char col_green[]       = "#56b88a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_green, col_green },
 };
 
 /* tagging */
@@ -59,7 +59,16 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = {
+    "dmenu_run",
+    "-m", dmenumon,
+    "-fn", dmenufont,
+    "-nb", col_gray1,
+    "-nf", col_gray3,
+    "-sb", col_green,
+    "-sf", col_gray4,
+    NULL
+};
 static const char *termcmd[]  = { "st", NULL };
 
 static Key keys[] = {

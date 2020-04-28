e385cf140ab40d9727e5dc92633844406225aefa
Gihyeon Sung <gi@hyeon.io>
2020-04-28 22:46:43 +0900

    Modify config.def.h
    - Revert font to Iosevka
    - Reduce snap pixel to 8px

--- config.def.h
+++ config.def.h
@@ -2,11 +2,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 8;        /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "NeoDunggeunmo:size=12", "monospace:size=10" };
-static const char dmenufont[]       = "NeoDunggeunmo:size=12";
+static const char *fonts[]          = { "Iosevka:size=12", "monospace:size=10" };
+static const char dmenufont[]       = "Iosevka:size=12";
 static const char col_gray1[]       = "#1c1c1c";
 static const char col_gray2[]       = "#1c1c1c";
 static const char col_gray3[]       = "#d0d0d0";

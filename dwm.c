6dfcb5e707001d15b1f0120a57fa632bbabf3fba
Gihyeon Sung <gi@hyeon.io>
2020-06-03 09:14:05 +0900

    Make status display only occupied tags

--- dwm.c
+++ dwm.c
@@ -716,6 +716,9 @@ drawbar(Monitor *m)
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
+    // skip drawing the tag if the tag has no windows
+    if (!(occ & 1 << i)) { continue; }
+
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
